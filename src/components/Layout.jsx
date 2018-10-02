bd9b0875796314d0496cc4b49b71656645a831ac
its-gio <me@its-g.io>
2018-10-01 17:41:30 -0700

    Fixed name of eslntrc and made lint check work

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Pets from "./Pets";
+import Pets from "./Pets.jsx";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pets name="Milk" animal="Dog" breed="Chiwawa" />;
+        <Pets name="Milk" animal="Dog" breed="Chiwawa" />
+        <Pets name="Cookie" animal="Dog" breed="Pit Bull" />
+        <Pets name="Boike" animal="Cat" breed="Mixed" />
       </div>
     );
   }
