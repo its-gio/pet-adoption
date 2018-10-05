0a9c2a8de373cbb6beebf5f47cc90e4409f83494
its-gio <me@its-g.io>
2018-10-05 12:19:24 -0700

    Setting up reach router

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Router } from "@reach/router";
 import Results from "./Results.jsx";
+import Details from "./Details.jsx";
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Results />
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
