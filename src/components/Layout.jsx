f1278f03337c2c9a5d1730bb994b2cc821262c19
its-gio <me@its-g.io>
2018-10-05 09:56:13 -0700

    Moving layout to Results component and rendering out resaults into layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,71 +1,12 @@
 import React from "react";
-import pf from "petfinder-client";
-import Pet from "./Pet.jsx";
+import Results from "./Results.jsx";
 
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: "full", location: "Los Angeles, CA" })
-      .then(data => {
-        let pets;
-
-        // Checking if these exist
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          // Making sure that the data will always be an array (Even if an empty array)
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-
-        this.setState({ pets });
-        // Example of 2 pet objects
-        // console.log(this.state.pets.slice(1, 3));
-      });
-  }
-
+export default class Layout extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-
-            // Checking and joining if there is only one breed
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(", ");
-            } else {
-              breed = pet.breeds.breed;
-            }
-
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <Results />
       </div>
     );
   }
---
+++ src/components/Results.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import pf from "petfinder-client";
+import Pet from "./Pet.jsx";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+export default class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Los Angeles, CA" })
+      .then(data => {
+        let pets;
+
+        // Checking if these exist
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          // Making sure that the data will always be an array (Even if an empty array)
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({ pets });
+        // Example of 2 pet objects
+        // console.log(this.state.pets.slice(1, 3));
+      });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => {
+          let breed;
+
+          // Checking and joining if there is only one breed
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(", ");
+          } else {
+            breed = pet.breeds.breed;
+          }
+
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city}, ${pet.contact.state}`}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
