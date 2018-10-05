47e60939e8b0a92a076109cccb0ec327bc7c52bc
its-gio <me@its-g.io>
2018-10-04 21:21:02 -0700

    Initializing state and adding api to play with (Pet Finder)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import pf from "petfinder-client";
 import Pets from "./Pets.jsx";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Los Angeles, CA" })
+      .then(data => {
+        let pets;
+
+        // Checking if these exist
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({ pets });
+      });
+  }
+
   render() {
     return (
       <div>
