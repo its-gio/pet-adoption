787b29ca7503609d6e81e2a23fe7d5ba999a8295
its-gio <me@its-g.io>
2018-10-09 12:34:43 -0700

    Adding animal options to form + two way binding with state

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ANIMALS } from "petfinder-client";
 
 export default class SearchParams extends React.Component {
   state = {
@@ -9,6 +10,10 @@ export default class SearchParams extends React.Component {
 
   handleLocationChange = e => {
     this.setState({ location: e.target.value });
+  };
+
+  handleAnimalChange = e => {
+    this.setState({ animal: e.target.value });
   };
 
   render() {
@@ -22,6 +27,22 @@ export default class SearchParams extends React.Component {
             value={this.state.location}
             placeholder="City... State..."
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.state.animal}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </div>
     );
