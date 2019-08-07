0e7c4fe881bf9e9042a7fdf97583aaf5bb16cbd0
Giovani Aguirre <me@its-g.io>
2019-08-06 22:13:05 -0700

    Added class and styles for single pet result

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Pet({ name, animal, breed }) {
   return (
-    <div>
+    <div className="results--single-pet">
       <h1>{name}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
