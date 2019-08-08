f2689a7eb966033e5ec640602cba70004aeeac33
Giovani Aguirre <me@its-g.io>
2019-08-07 19:43:53 -0700

    Recreated single pet display. created small function to capitalize names.

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
+
+  const nameCase = name => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  };
+
   return (
     <div className="results--single-pet">
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-      <hr />
+      <a href={`/details/${id}`}>
+        <img src={hero} alt={name} />
+      </a>
+
+      <div className="results--single-pet__info">
+        <h3>{nameCase(name)}</h3>
+        <p>{animal}</p>
+        <p>{breed}</p>
+        <p>{location}</p>
+      </div>
     </div>
   );
 }
