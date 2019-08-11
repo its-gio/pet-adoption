fd7390c9d7f3853746393c476e2fff3ecc6ef478
Giovani Aguirre <me@its-g.io>
2019-08-11 13:26:11 -0700

    Created state component with and setstate after running API call plus a small loader on loading

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import pet from "@frontendmasters/pet";
 
 export default class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+  // I'm going to do this once then I'm done (Usefull for ajax requests)
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+
+        loading: false
+      });
+    }, console.error);
+  }
+
   render() {
-    return <div></div>;
+    if (this.loading) {
+      return <h1>Loading...</h1>;
+    } else {
+      return <div>Loadng complete!</div>;
+    }
   }
 }
